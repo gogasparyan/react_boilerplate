5caca293ea507ce8b0b79e32dedb883805b7f334
Gor Gasparyan <20324876+gogasparyan@users.noreply.github.com>
2018-06-28 23:33:59 +0400

    Initial commit

---
+++ src/components/main.js
@@ -0,0 +1,20 @@
+//import React, { Component } from 'react';
+//import
+//
+//
+//class Main extends Component {
+//    render() {
+//        return (
+//        <div id="item">
+//        <div className="items"><img src="" alt=""></div>
+//        <div className="items"><img src="" alt=""></div>
+//        <div className="items"><img src="" alt=""></div>
+//        <div className="items"><img src="" alt=""></div>
+//        <div className="items"><img src="" alt=""></div>
+//        <div className="items"><img src="" alt=""></div>
+//        <div className="items"><img src="" alt=""></div>
+//        <div className="items"><img src="" alt=""></div>
+//    </div>
+//        )
+//    }
+//}
